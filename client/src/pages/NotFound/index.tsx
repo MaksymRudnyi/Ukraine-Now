4f26785ac74ba56649a4e596f580542c8d6b7bbd
Maksym Rudnyi <mrudnyi@idiq.com>
2022-11-17 20:02:43 +0200

    added router

---
+++ client/src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+import { FC } from 'react';
+
+export const NotFound: FC = () => (
+  <Box
+    backgroundColor={['red', 'blue', 'green', 'yellow', 'pink']}
+    className="App"
+  >
+    NotFound page
+  </Box>
+);
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { About } from './About';
+import { Home } from './Home';
+import { NotFound } from './NotFound';
+
+export { Home, About, NotFound };
