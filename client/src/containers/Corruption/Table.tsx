97871d85e0f8908cab066f4f2d6c936e1272a727
Maksym Rudnyi <mrudnyi@idiq.com>
2022-12-18 17:55:32 +0200

    not finished table

--- client/src/containers/Corruption/Table.tsx
+++ client/src/containers/Corruption/Table.tsx
@@ -1,60 +1,57 @@
+import { Table } from '../../components/Table';
 import { GetCorruption_corruption } from './__generated__/GetCorruption';
-import { Box, TableContainer, Table,Thead, Tr, Th, Td, Tbody } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { observer } from 'mobx-react-lite';
 import { FC, useRef, useEffect } from 'react';
 
-
 type CurruptionTableProps = {
   corruption: GetCorruption_corruption[];
+  onRowClick?: Function;
 };
 
 export const CorruptionTable: FC<CurruptionTableProps> = observer(
-  ({ corruption }) => {
+  ({ corruption, onRowClick }) => {
     const ukraineRef = useRef<HTMLTableRowElement>(null);
 
     useEffect(() => {
-      if(ukraineRef.current) {
+      if (ukraineRef.current) {
         ukraineRef.current.scrollIntoView();
       }
-    }, [])
+    }, []);
+
+    const columns = [
+      {
+        id: 'score',
+        accessorKey: 'score',
+        header: 'Score',
+      },
+      {
+        id: 'country',
+        accessorKey: 'country',
+        header: 'Country',
+        cell: (info) => info.getValue(),
+        align: 'right',
+      },
+      {
+        id: 'rank',
+        accessorKey: 'rank',
+        header: 'Rank',
+        cell: (info) => info.getValue(),
+      },
+    ];
+
+    const selectedFn = (row) => {
+      return row.iso3 === 'UKR';
+    };
+
     return (
-      <Box >
-        <TableContainer>
-          <Table variant='simple'
-                 size={'sm'}
-                 mt={'32px'}
-                 display={'inline-block'}
-                 overflow={'auto'}
-                 sx={{maxHeight: '300px'}}>
-            <Thead>
-              <Tr>
-                <Th isNumeric>
-                  <Box position={'absolute'} mt={'-28px'}>Score</Box>
-                </Th>
-                <Th>
-                  <Box position={'absolute'} mt={'-28px'}>Country</Box>
-                </Th>
-                <Th isNumeric>
-                  <Box position={'absolute'} mt={'-28px'}>Rank</Box>
-                </Th>
-              </Tr>
-            </Thead>
-
-            <Tbody maxHeight={'300px'}>
-              {corruption.map((country) => (
-                <Tr key={country.country}
-                    bgColor={country.iso3 === 'UKR' ? 'blue.200' : 'none'}
-                    ref={country.iso3 === 'UKR' ? ukraineRef : null}
-                >
-                  <Td isNumeric>{country.score}</Td>
-                  <Td>{country.country}</Td>
-                  <Td isNumeric>{country.rank}</Td>
-                </Tr>
-              ))}
-            </Tbody>
-
-          </Table>
-        </TableContainer>
+      <Box>
+        <Table
+          data={corruption}
+          columns={columns}
+          selectedFn={selectedFn}
+          onRowClick={onRowClick}
+        />
       </Box>
     );
   }
