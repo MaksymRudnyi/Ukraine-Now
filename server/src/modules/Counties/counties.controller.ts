d7abf63e58317bdbfd0809ea62987389c2a72c33
Maksym Rudnyi <mrudnyi@idiq.com>
2023-02-15 15:15:20 +0200

    add endpoint with countries

--- server/src/modules/Counties/counties.controller.ts
+++ server/src/modules/Counties/counties.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 
+// Taken from https://stefangabos.github.io/world_countries/
 import * as uk from './locals/uk.json';
 import * as en from './locals/en.json';
 
 @Controller('countries')
 export class CountiesController {
-  constructor() {}
-
   @Get()
   exchange(@Res() res: Response, @Query() query: { locale: string }) {
     res.status(HttpStatus.OK).json(query.locale === 'en' ? en : uk);
