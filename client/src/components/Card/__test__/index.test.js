a3764b2d519dde9d01b583845ffa05fe0523f6c5
ksenia-b <ksenia.barshchyk@gmail.com>
2023-04-03 20:28:23 +0300

    added test for checking if 'helpText' is rendering.

--- client/src/components/Card/__test__/index.test.js
+++ client/src/components/Card/__test__/index.test.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { getByText, render, screen } from '@testing-library/react';
 import { Card } from '../index';
 
 describe('Card', () => {
   it('should render the title and value', () => {
-    const title = 'Test Title';
+    const title = 'Test title';
     const value = 42;
+  
     const { getByText } = render(<Card title={title} value={value} />);
-
     expect(getByText(title)).toBeInTheDocument();
     expect(getByText(String(value))).toBeInTheDocument();
+
   });
 
+  it('should NOT render the help text', () => {
+    const title = 'Test title';
+    const value = 42;
+    const helpText = 'Help text1';
+
+    render(<Card title={title} value={value} helpText={helpText} />);
+  
+    const headingElement = screen.getByText(helpText);
+    expect(headingElement).not.toBeNull();
+  });
+
+  
   it('should render the help text if provided', () => {
     const helpText = 'Test Help Text';
     const { getByText } = render(<Card title="Test Title" value={null} helpText={helpText} />);
@@ -19,3 +32,6 @@ describe('Card', () => {
     expect(getByText(helpText)).toBeInTheDocument();
   });
 });
+
+
+
